a61ef7d412522ce143cd0cc5d1719480b2d15382
BoondockSulfur <barth.steffen@proton.me>
2026-05-04 20:15:42 +0200

    Fix missing import java.net.URL and add v2.2.0/v2.2.1 changelog entries

--- src/main/java/de/boondocksulfur/customjukebox/utils/UpdateChecker.java
+++ src/main/java/de/boondocksulfur/customjukebox/utils/UpdateChecker.java
@@ -10,6 +10,7 @@ import org.bukkit.Bukkit;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CompletableFuture;
